5b149fc273b1947312c372e23bbd87a224c6b288
Adespscientist <79201846+Adespscientist@users.noreply.github.com>
2022-11-25 08:50:32 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['cdn.pixabay.com', 'c4.wallpaperflare.com', 'pixabay.com'],
+  },
 }
+
---
+++ pages/components/Content.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Content = () => {
+  return (
+    <div className="flex">
+     <div>Text1</div>
+     <div>Text2</div>
+      </div>
+  )
+}
+
+export default Content
