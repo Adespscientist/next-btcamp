46bfd18f4e6e0a236d9ba7e19370d86fe2afc5a0
Adespscientist <79201846+Adespscientist@users.noreply.github.com>
2022-11-25 09:18:22 +0100

    s

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,10 @@ const Home: NextPage = () => {
   return (
      <div>
         <div className="relative mx-0 top-0 mr-auto flex justify-end">
-        <Image src="https://github.com/Adespscientist/next-btcamp/blob/main/public/asset.png?raw=true" className="imageBanner"
-        width={100} height={100}alt=""/>
+        <Image src="https://github.com/Adespscientist/next-btcamp/blob/main/public/hero%201.png?raw=true" className="imageBanner"
+        width={100} height={100}
+        quality={100}
+        alt=""/>
         </div>
       <header>
       {/* Navbar Here */}
